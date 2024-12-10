958b45a4a039ae052f9223f5ce2cc521605d8333
Gene-Weaver <willwe@umich.edu>
2024-12-10 14:07:14 -0500

    autoupdate

--- vouchervision/fetch_data.py
+++ vouchervision/fetch_data.py
@@ -5,11 +5,20 @@ from urllib.request import urlopen
 from zipfile import ZipFile
 import urllib.request
 from tqdm import tqdm
+import ssl
+import certifi
 import subprocess
 
 VERSION = 'v-2-2'
 
 def fetch_data(logger, dir_home, cfg_file_path):
+    # Force Python to use certifi's CA bundle
+    os.environ['SSL_CERT_FILE'] = certifi.where()
+    os.environ['REQUESTS_CA_BUNDLE'] = certifi.where()
+
+    # Create an SSL context that uses certifi's CA bundle
+    ssl_context = ssl.create_default_context(cafile=certifi.where())
+
     logger.name = 'Fetch Data'
     ready_to_use = False
     do_fetch = True
